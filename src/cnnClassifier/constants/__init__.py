766777a7d5fdb4285dffcb9d375b4a9a15a1710c
Troy Zhang <troyz0213@gmail.com>
2024-08-16 11:50:47 -0700

    begin data ingestion

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path 
+
+# initialize variablest to config.yaml and params.yaml so we don't need to specify the path every time we want to access them
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
