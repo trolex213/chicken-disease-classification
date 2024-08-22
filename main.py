a298424238ce87cffe2facbf22e88f0932061406
Troy Zhang <troyz0213@gmail.com>
2024-08-22 12:18:14 -0700

    completed main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 import sys
-print(f"Current working directory: {os.getcwd()}")
-print(f"Python path: {sys.path}")
 
 # Add the project root to the Python path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
-print(f"Updated Python path: {sys.path}")
 
 # Rest of your imports and code
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
+
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline 
+
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     data_ingestion = DataIngestionPipeline()
     logger.info(f"Starting stage {STAGE_NAME}.")
@@ -23,3 +23,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare Base Model stage" 
+try:
+    prepare_base_model = PrepareBaseModelPipeline()
+    logger.info(f"Starting stage {STAGE_NAME}.")
+    prepare_base_model.main()
+    logger.info(f"Completed stage {STAGE_NAME}.")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+
