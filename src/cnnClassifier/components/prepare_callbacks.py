004ebcc1845f9c64b34aca5ea5e3c7899bd7b5ed
Troy Zhang <troyz0213@gmail.com>
2024-08-23 10:49:20 -0700

    prepare_callbacks in components completed

---
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -0,0 +1,33 @@
+import urllib.request as request 
+import zipfile as ZipFile
+import tensorflow as tf
+import time 
+import os 
+from cnnClassifier.entity.config_entity import PrepareCallbacksConfig
+
+class PrepareCallbacks:
+    def __init__(self, config: PrepareCallbacksConfig):
+        self.config = config 
+
+    @property
+    def create_tb_callbacks(self):
+        timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        tb_running_log_dir = os.path.join(
+            self.config.tensorboard_root_log_dir,
+            f"tb_logs_at_{timestamp}",
+        )
+
+        return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
+    
+    @property
+    def create_ckpt_callbacks(self):
+        return tf.keras.callbacks.ModelCheckpoint(
+            filepath=str(self.config.checkpoint_model_filepath),
+            save_best_only=True
+        )
+    
+    def get_tb_ckpt_callbacks(self):
+        return [
+            self.create_tb_callbacks,
+            self.create_ckpt_callbacks
+        ]
